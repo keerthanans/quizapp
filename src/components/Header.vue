252ff842c169404508a2a2e1bf0f423a94ee9d99
keerthanans <k.sudalaimani@perficient.com>
2020-06-05 19:06:28 +0530

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item active>Fancy Quiz</b-nav-item>
+      <b-nav-item>counter:{{numcorrect}}/{{numTotal}}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+<script>
+export default{
+  props:[
+    'numcorrect',
+    'numTotal'
+  ]
+}
+</script>
